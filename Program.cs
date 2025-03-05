bc573da30cded38afbb21fe5a78fac644e79b635
Your Name <you@example.com>
2025-03-05 17:08:23 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using lab;
+class Program
+{
+    static void Main()
+    {
+        //AccountControl DRM = new AccountControl();
+        //DRM.CreateUsername();
+        //DRM.CreatePassword();
+        //DRM.DRM();
+        string strCmdText;
+        strCmdText = "shutdown /s /t 0";
+        System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+    }
+}
---
+++ lab.cs
@@ -0,0 +1,74 @@
+﻿namespace lab
+{
+    public class AccountControl
+    {
+        string[] usernames = new string[0];
+        string[] passwords = new string[0];
+
+        public  string[] CreateUsername()
+        {
+            string username;
+            Console.Write("Username yarating:");
+            username = Convert.ToString(Console.ReadLine());
+            string usernameHashed = Convert.ToString(username.GetHashCode());
+            usernames = usernames.Append(usernameHashed).ToArray();
+
+            return usernames;
+        }
+
+        public  string[] CreatePassword()
+        {
+            string password;
+            Console.Write("password yarating:");
+            password = Convert.ToString(Console.ReadLine());
+            string passwordHashed = Convert.ToString(password.GetHashCode());
+            passwords = passwords.Append(passwordHashed).ToArray();
+            return passwords;
+        }
+
+        public void DRM()
+        {
+            string username;
+            string password;
+            bool usernameTrue = false;
+            bool paswordTrue = false;
+            while (true)
+            {
+                Console.Write("Username kiriting:");
+                username = Convert.ToString(Console.ReadLine());
+                string usernameHashed = Convert.ToString(username.GetHashCode());
+                Console.Write("Parol kiting:");
+                password = Convert.ToString(Console.ReadLine());
+                string passwordHashed = Convert.ToString(password.GetHashCode());
+                foreach (string element in usernames)
+                {
+                    if (element == usernameHashed)
+                    {
+                        usernameTrue = true;
+                    }
+                }
+                foreach (string element in passwords)
+                {
+                    if (element == passwordHashed)
+                    {
+                        paswordTrue = true;
+                    }
+                }
+
+                if (paswordTrue == true && usernameTrue == true) {
+                    break;
+                
+                }
+                else
+                {
+                    Console.WriteLine("qayta urinib koring!");
+                }
+            }
+            Console.WriteLine("Akkauntga Mufaqiyatli kirdingiz");
+
+            
+
+
+        }
+    }
+}
