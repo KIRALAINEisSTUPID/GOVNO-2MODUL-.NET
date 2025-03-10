7ce7f21373e8105d3a7952b8e916465a5aa6963e
Your Name <you@example.com>
2025-03-10 17:13:22 +0500

    updated

--- Polymorphism/main.cs
+++ Polymorphism/main.cs
@@ -1,29 +1,35 @@
 ﻿using System;
+internal class Person
+{
 
+
+}
 class MainClass
 {
-    public int Calculate(int a, int b)
-    {
-        return a + b;
-    }
+    //public int Calculate(int a, int b)
+    //{
+    //    return a + b;
+    //}
 
-    public int Calculate(int a, int b, char op)
-    {
-        return op switch
-        {
-            '-' => a - b,
-            '*' => a * b,
-            '/' => a/b        
-        };
-    }
+    //public int Calculate(int a, int b, char op)
+    //{
+    //    return op switch
+    //    {
+    //        '-' => a - b,
+    //        '*' => a * b,
+    //        '/' => a/b        
+    //    };
+    //}
 
-    static void Main(string[] args)
-    {
-        MainClass calc = new MainClass();
+    //static void Main(string[] args)
+    //{
+    //    MainClass calc = new MainClass();
 
-        Console.WriteLine("10 + 5 = " + calc.Calculate(10, 5));
-        Console.WriteLine("10 - 5 = " + calc.Calculate(10, 5, '-'));
-        Console.WriteLine("10 * 5 = " + calc.Calculate(10, 5, '*'));
-        Console.WriteLine("10 / 5 = " + calc.Calculate(10, 5, '/'));
-    }
+    //    Console.WriteLine("10 + 5 = " + calc.Calculate(10, 5));
+    //    Console.WriteLine("10 - 5 = " + calc.Calculate(10, 5, '-'));
+    //    Console.WriteLine("10 * 5 = " + calc.Calculate(10, 5, '*'));
+    //    Console.WriteLine("10 / 5 = " + calc.Calculate(10, 5, '/'));
+    //}
+
+    
 }
